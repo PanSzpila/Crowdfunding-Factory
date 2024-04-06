118503f11ff0e4579ce76eb8a73af6c375b705a6
Piotr Podgórski <p.t.podgorski+git@gmail.com>
2024-04-06 22:50:01 +0200

    initial commit

---
+++ ethereum/compile.js
@@ -0,0 +1,46 @@
+import path from "path";
+import fs from "fs-extra";
+import solc from "solc";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const buildPath = path.resolve(__dirname, "build");
+const CrowdfundingPath = path.resolve(
+  __dirname,
+  "contracts",
+  "Crowdfunding.sol"
+);
+const source = fs.readFileSync(CrowdfundingPath, "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "Crowdfunding.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const compiledContracts = JSON.parse(
+  solc.compile(JSON.stringify(input))
+).contracts;
+
+fs.ensureDirSync(buildPath);
+
+const file = "Crowdfunding.sol";
+for (let contractName in compiledContracts[file]) {
+  fs.outputJSONSync(
+    path.resolve(buildPath, contractName + ".json"),
+    compiledContracts[file][contractName]
+  );
+}
+
+export const { abi, evm } = compiledContracts["Crowdfunding.sol"].Crowdfunding;
---
+++ ethereum/contracts/Crowdfunding.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract CrowdfundingFactory{
+    address[] public deployedCrowdfundings;
+
+    function createCrowdfunding(uint minimumContribution) public {
+        address newCrowdfunding = address(new Crowdfunding(minimumContribution, msg.sender));
+        deployedCrowdfundings.push(newCrowdfunding);
+    }
+    function getDeployedCrowdfundings() public view returns (address[] memory){
+        return deployedCrowdfundings;
+    }
+}
+
+contract Crowdfunding {
+    struct Request {
+        string description;
+        uint256 value;
+        address payable recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint256 public minimumContribution;
+    mapping (address => bool) public approvers;
+    uint public approversCount;
+
+    modifier onlyToManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint256 minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public onlyToManager {
+        // Request memory NewRequest = Request ({ //Request - tworzymy obiekt (struct) o typie danych zdefiniowanych na początku kontraktu jako Request. Ta zmienna będzie się nazywać NewRequest i będzie się równać
+        //     description: description,
+        //     value: value,
+        //     recipient: recipient,
+        //     complete: false,
+        //     approvalCount: 0
+        // });
+        uint256 _index = requests.length;
+        Request[] storage r = requests;
+        r.push();
+
+        r[_index].description = description;
+        r[_index].value = value;
+        r[_index].recipient = recipient;
+        r[_index].complete = false;
+        r[_index].approvalCount = 0;
+
+        // requests.push(NewRequest);
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 i) public onlyToManager {
+        Request storage request = requests[i]; //const request = requests[i], typ Request (wcześniej zdefiniowany),  miejsce przechowywania: storage
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
