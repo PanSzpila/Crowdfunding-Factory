75851592f31948356672dbfef66fd1137842496d
Piotr Podgórski <p.t.podgorski+git@gmail.com>
2024-04-18 22:54:47 +0200

    test of requests process

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -4,6 +4,7 @@ import { beforeEach, it } from "mocha";
 import { Web3 } from "web3";
 import compiledFactory from "../ethereum/build/CrowdfundingFactory.json" assert { type: "json" };
 import compiledCrowdfunding from "../ethereum/build/Crowdfunding.json" assert { type: "json" };
+import { parse } from "path";
 
 const web3 = new Web3(
   ganache.provider({
@@ -37,7 +38,6 @@ beforeEach(async () => {
     console.error(error);
   }
 
-  // console.log("factory", factory);
   [crowdfundingAddress] = await factory.methods
     .getDeployedCrowdfundings()
     .call(); //destructurisation - take first element of array and assaign it for variable crowdfundingAdress
@@ -100,110 +100,31 @@ describe("Crowdfundings", () => {
       })
       .on("error", console.error);
   });
-  // it ('allows a manager to make a payment request', async () => {
 
-  // })
+  it("processes requests", async () => {
+    await crowdfunding.methods
+      .contribute()
+      .send({ from: accounts[0], value: web3.utils.toWei("10", "ether") });
+
+    await crowdfunding.methods
+      .createRequest("A", web3.utils.toWei("5", "ether"), accounts[1])
+      .send({ from: accounts[0], gas: "1000000" });
+
+    await crowdfunding.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    await crowdfunding.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    balance = parseFloat(balance);
+    console.log("balance", balance);
+
+    assert(balance > 104);
+  });
 });
-
-// describe("Lottery contract", () => {
-//   it("deploys a contract", () => {
-//     assert.ok(lottery.options.address);
-//   });
-
-//   it("allows one account to enter", async () => {
-//     await lottery.methods.enter().send({
-//       from: accounts[0],
-//       value: web3.utils.toWei("1", "finney"),
-//     });
-
-//     const players = await lottery.methods.getPlayers().call({
-//       from: accounts[0],
-//     });
-
-//     assert.equal(accounts[0], players[0]);
-//     assert.equal(1, players.length);
-//   });
-
-//   it("allows multiple accounts to enter", async () => {
-//     await lottery.methods.enter().send({
-//       from: accounts[0],
-//       value: "1000000000000000",
-//     });
-//     await lottery.methods.enter().send({
-//       from: accounts[1],
-//       value: "1000000000000000",
-//     });
-//     await lottery.methods.enter().send({
-//       from: accounts[2],
-//       value: "1000000000000000",
-//     });
-
-//     const players = await lottery.methods.getPlayers().call({
-//       from: accounts[0],
-//     });
-
-//     assert.equal(accounts[0], players[0]);
-//     assert.equal(accounts[1], players[1]);
-//     assert.equal(accounts[2], players[2]);
-//     assert.equal(3, players.length);
-//   });
-
-//   it("requires a minimum amount of ether to enter", async () => {
-//     try {
-//       await lottery.methods.enter.send({
-//         from: accounts[0],
-//         value: "100000000000009",
-//       });
-//       assert(false);
-//     } catch (err) {
-//       assert(err);
-//     }
-//   });
-
-//   it("only manager can call pickWinner", async () => {
-//     try {
-//       await lottery.methods.pickwinner().send({
-//         from: accounts[1],
-//       });
-//       assert(false);
-//     } catch (err) {
-//       assert(err);
-//     }
-//   });
-
-//   it("sends money to the winner and resets the players array", async () => {
-//     await lottery.methods.enter().send({
-//       from: accounts[0],
-//       value: web3.utils.toWei("1", "finney"),
-//     });
-//     const initialBalance = await web3.eth.getBalance(accounts[0]);
-//     await lottery.methods.pickWinner().send({ from: accounts[0] });
-//     const finalBalance = await web3.eth.getBalance(accounts[0]);
-//     const difference = finalBalance - initialBalance;
-
-//     assert(difference > web3.utils.toWei("0.9", "finney"));
-//   });
-
-//   it("clears array of players after win", async () => {
-//     await lottery.methods.enter().send({
-//       from: accounts[0],
-//       value: "1000000000000000",
-//     });
-//     await lottery.methods.enter().send({
-//       from: accounts[1],
-//       value: "1000000000000000",
-//     });
-//     await lottery.methods.enter().send({
-//       from: accounts[2],
-//       value: "1000000000000000",
-//     });
-
-//     await lottery.methods.pickWinner().send({ from: accounts[0] });
-
-//     const players = await lottery.methods.getPlayers().call({
-//       from: accounts[0],
-//     });
-
-//     assert.equal(0, players.length);
-//   });
-// });
